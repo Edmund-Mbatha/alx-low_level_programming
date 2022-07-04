97111632c73aa74dc6ead32cb7b5c5037f7adf3d
Edy Mbatha <Edmundmbatha0@gmail.com>
2022-07-04 16:28:32 -0700

    3&4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	char ch = 'a', CH = 'A';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar (ch);
+	for (CH ='A'; CH <= 'Z'; CH++)
+	putchar (CH);
+	putchar ('\n');
+	return (0);
+}
