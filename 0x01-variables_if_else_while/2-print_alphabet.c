9dc7171d8dd5dd245b14b939e72674862ce3ce5d
Edy Mbatha <Edmundmbatha0@gmail.com>
2022-07-04 15:36:03 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
 	char ch = 'a';
-	for(ch ='a'; ch <= 'z'; ch++)
-	putchar(ch);
-	putchar('\n');
+
+	for(ch = 'a'; ch <= 'z'; ch++)
+	putchar (ch);
+	putchar ('\n');
 	return (0);
 }
 
