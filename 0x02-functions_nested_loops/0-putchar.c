581cf84ee360899eb310e76054ca690e8f6de993
Edy Mbatha <Edmundmbatha0@gmail.com>
2022-07-06 15:01:24 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * printputchar - prints "putchar"
+ *
+ * return: nothing
+ */
+void printputchar(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	-putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+}
