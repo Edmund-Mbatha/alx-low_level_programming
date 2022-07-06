e9e34f815e4a605e710e9f73126d24924e3d7e3b
Edy Mbatha <Edmundmbatha0@gmail.com>
2022-07-06 16:52:22 -0700

    1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet in lower case ten times
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i = 0;
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
